d832d9170cf94ff39dcda9a53db4fcc66d06e08d
kamesa7 <35834522+kamesa7@users.noreply.github.com>
2019-08-04 14:11:04 +0900

    quarry

--- digger.js
+++ digger.js
@@ -2,11 +2,14 @@
 glob.digBlockAt = digBlockAt
 glob.stopDigging = stopDigging
 glob.mining = mining
+glob.quarry = quarry
 
 glob.logDig = false
 
-glob.miningInterval = 150
-glob.miningWorkProgress = 50
+glob.diggingInterval = 150
+glob.diggingWorkProgress = 50
+
+var digger;
 
 const shovel = [277, 256, 273, 269, 284]
 const pickaxe = []
@@ -68,8 +71,7 @@ function digBlockAt(pos, cb = noop) {
         case "wool": tool = glob.findItem(shears)
             break
         default:
-            cb("[dig] invalid block material")
-            return
+            bot.log("[dig] invalid block material")
     }
     if (tool) {
         bot.equip(tool, "hand", (err) => {
@@ -113,16 +115,22 @@ bot.on('death', () => {
 
 function stopDigging() {
     glob.stopMoving()
-    if (miner && !miner._destroyed) {
-        bot.log("[dig] stop digging : mined " + minedLength + "m")
+    if (digger && !digger._destroyed) {
+        if (minedLength > 0) {
+            bot.log("[dig] stop digging : mined " + minedLength + "m")
+            minedLength = 0
+        }
+        if (quarryBlocks > 0) {
+            bot.log("[dig] stop digging : quarry " + quarryBlocks + "blocks")
+            quarryBlocks = 0
+        }
     }
-    clearInterval(miner)
+    clearInterval(digger)
 }
 
-var miner;
-var minedLength;
+var minedLength = 0;
 function mining(d, length = 100) {
-    clearInterval(miner)
+    stopDigging()
     var direction
     switch (d) {
         case "N": case "n": direction = new Vec3(0, 0, -1)
@@ -143,7 +151,7 @@ function mining(d, length = 100) {
     var digging = new Vec3(0, 0, 0);
     minedLength = 0;
     var prevMinedLength = 0;
-    miner = setInterval(miningControl, glob.miningInterval)
+    digger = setInterval(miningControl, glob.diggingInterval)
     function miningControl() {
         switch (miningState) {
             case "move":
@@ -225,14 +233,14 @@ function mining(d, length = 100) {
                             digging.add(direction)
                             minedLength = Math.floor(digging.norm())
                             if (minedLength > length) {
-                                clearInterval(miner)
+                                stopDigging()
                                 bot.log("[mining] Mining Finished  " + length)
                                 return
                             }
                         }
                     }
                 }
-                if (minedLength - prevMinedLength >= glob.miningWorkProgress) {
+                if (minedLength - prevMinedLength >= glob.diggingWorkProgress) {
                     bot.log("[mining] Mining " + minedLength + "m " + Math.floor(100 * minedLength / length) + "% at " + bot.entity.position.floored())
                     prevMinedLength = minedLength
                 }
@@ -259,6 +267,84 @@ function mining(d, length = 100) {
     }
 }
 
+var quarryBlocks = 0
+function quarry(size, origin = bot.entity.position.offset(0, 1, 0)) {
+    origin.floor()
+    bot.log("[quarry] Quarry Start  size " + size + " at " + origin)
+    var depth = 0
+    var digging = quarryNext()
+    var quarryState = "move"
+    var sizeSum = size.x * size.y * size.z
+    quarryBlocks = 0
+    digger = setInterval(quarryControl, glob.diggingInterval)
+    function quarryControl() {
+        switch (quarryState) {
+            case "move":
+                if (!glob.doNothing()) return
+                glob.goToPos(origin.plus(digging), {
+                    ignore: !glob.logDig,
+                    allowGoal: 3,
+                    standadjust: 0,
+                    bridgeable: true,
+                    buildstairable: true,
+                    scaffordable: true,
+                    strictfin: true,
+                    continue: false,
+                })
+                quarryState = "quarry"
+                break;
+            case "quarry":
+                if (!glob.doNothing()) return
+                glob.queueOnceState("quarry", digBlockAt, origin.plus(digging), (err) => {
+                    if (err) bot.log(err)
+                    else quarryBlocks++;
+
+                    if (quarryBlocks % glob.diggingWorkProgress == 0) {
+                        bot.log("[quarry] Quarry " + quarryBlocks + "blocks " + Math.floor(100 * quarryBlocks / sizeSum) + "% at " + bot.entity.position.floored())
+                    }
+
+                    digging = quarryNext()
+                    if (digging == null) {
+                        bot.log("[quarry] Quarry Finished")
+                        stopDigging()
+                        glob.finishState("quarry")
+                        return
+                    }
+                    quarryState = "move"
+                    glob.finishState("quarry")
+                })
+                break;
+        }
+    }
+    function quarryNext() {
+        let nearest = null
+        const distDigging = bot.entity.position.floored().minus(origin)
+        for (let y = depth; y > -size.y; y -= 2) {
+            depth = y
+            for (let z = 0; z < size.z; z++) {
+                for (let x = 0; x < size.x; x++) {
+                    let pos1 = new Vec3(x, y, z)
+                    let pos2 = new Vec3(x, y - 1, z)
+                    if (!nearest || pos1.distanceTo(distDigging) < nearest.distanceTo(distDigging)) {
+                        let block1 = bot.blockAt(origin.plus(pos1))
+                        if (block1 && block1.boundingBox != "empty") {
+                            nearest = pos1;
+                        } else if (!nearest || pos2.distanceTo(distDigging) < nearest.distanceTo(distDigging)) {
+                            let block2 = bot.blockAt(origin.plus(pos2))
+                            if (block2 && block2.boundingBox != "empty")
+                                nearest = pos2;
+                        }
+                    }
+                }
+            }
+            if (nearest) {
+                return nearest
+            }
+            if (y - 2 == size.y) y++;
+        }
+    }
+    return null
+}
 
 function noop(err) {
     //do nothing special
